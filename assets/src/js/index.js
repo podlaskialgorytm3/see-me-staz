4706e92b5dd13c18aabcdb0fcba4392d672bf9b4
podlaski <live.m851@gmail.com>
2022-07-16 22:57:42 +0200

    Category section update

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,7 +1,7 @@
-// window.addEventListener('scroll', () => {
-//     const nav = document.querySelector('.nav-menu')
-//     nav.classList.toggle('sticky',window.scrollY > 108)
-// })
+ window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav-menu')
+     nav.classList.toggle('sticky',window.scrollY > 108)
+ })
 
 const menu = document.querySelector('nav');
 
